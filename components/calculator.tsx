656278e51c5e8f6105b12ea7c92e9c1096fb1cef
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-05-17 21:14:14 -0300

    feat: create span of `RS` above input

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -10,21 +10,26 @@ import {
 
 export function Calculator() {
   return (
-    <section className="w-full rounded-sm bg-white p-10 lg:h-[467px] lg:w-[426px]">
+    <section className="w-full rounded-sm bg-white p-8 lg:h-[467px] lg:w-[426px]">
       <h2 className="mb-6 text-[17px] font-medium leading-[26px] text-black">
         Calculadora de Férias Online
       </h2>
-      <div className="form grid grid-cols-2 gap-x-4">
+      <div className="form grid grid-cols-2 gap-x-4 text-black">
         <div className="">
           <Label>Salário Bruto</Label>
-          <Input defaultValue={"0,00"} />
+          <div className="relative">
+            <div className="boder-[#CECED8] absolute inset-0 z-0 my-[1px] ml-[1px] flex h-[38px] w-10 items-center justify-center rounded border-0 border-y border-l bg-[#EEF2F8]">
+              <span>R$</span>
+            </div>
+            <Input className="z-10 pl-11" defaultValue={"0,00"} />
+          </div>
 
           <Label>Dependentes</Label>
           <Input defaultValue={"0"} />
 
           <Label>Abono pecuniário 1/3</Label>
           <Select>
-            <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="">
               <SelectValue placeholder="Theme" />
             </SelectTrigger>
             <SelectContent>
@@ -43,7 +48,7 @@ export function Calculator() {
 
           <Label>Adiantar 13 parcela</Label>
           <Select>
-            <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="">
               <SelectValue placeholder="Theme" />
             </SelectTrigger>
             <SelectContent>
