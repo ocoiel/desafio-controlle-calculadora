02e4ca6a3fe583e51498a430cfecaced55b2c6f2
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-05-20 17:51:49 -0300

    chore: prepare to use radix-form with rhf and tailwindcss

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -1,7 +1,7 @@
+"use client"
+
+import React, { useState } from "react"
 import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
 
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
@@ -20,106 +20,117 @@ import {
   TooltipTrigger,
 } from "./ui/tooltip"
 
-export const calculatorSchema = z.object({})
-
-export type Calculator = z.infer<typeof calculatorSchema>
-
 export function Calculator() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    mode: "onChange",
-    resolver: zodResolver(calculatorSchema),
-  })
-
   return (
     <section className="w-full rounded-sm bg-white p-8 lg:h-[467px] lg:w-[426px]">
-      <h2 className="mb-6 text-[17px] font-medium leading-[26px] text-black">
-        Calculadora de Férias Online
-      </h2>
-      <div className="form grid grid-cols-2 gap-x-4 text-black">
-        <div className="grid gap-y-4">
-          <div>
-            <Label>Salário Bruto</Label>
-            <div className="relative">
-              <div className="boder-[#CECED8] absolute inset-0 z-0 my-[1px] ml-[1px] flex h-[38px] w-10 items-center justify-center rounded border-0 border-y border-l bg-[#EEF2F8]">
-                <span className="text-[13px]">R$</span>
+      <form>
+        <h2 className="mb-6 text-[17px] font-medium leading-[26px] text-black">
+          Calculadora de Férias Online
+        </h2>
+        <div className="form grid grid-cols-2 gap-x-4 text-black">
+          <div className="grid gap-y-4">
+            <div>
+              <Label>Salário Bruto</Label>
+              <div className="relative">
+                <div className="boder-[#CECED8] absolute inset-0 z-0 my-[1px] ml-[1px] flex h-[38px] w-10 items-center justify-center rounded border-0 border-y border-l bg-[#EEF2F8]">
+                  <span className="text-[13px]">R$</span>
+                </div>
+                <Input
+                  name="salary"
+                  type="number"
+                  required
+                  className="z-10 pl-11"
+                  placeholder="0,00"
+                />
               </div>
-              <Input className="z-10 pl-11" defaultValue={"0,00"} />
+            </div>
+
+            <div>
+              <Label>Dependentes</Label>
+              <Input name="deps" defaultValue={"0"} />
+            </div>
+
+            <div>
+              <TooltipProvider delayDuration={50}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Label>Abono pecuniário 1/3</Label>
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-[300px] text-sm">
+                    <p>
+                      Significa que você pode vender um terço (1/3) de suas
+                      férias para a empresa, recebendo uma remuneração a mais
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+
+              <Select name="abono" defaultValue="no">
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Não" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="no">Não</SelectItem>
+                  <SelectItem value="yes">Sim</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
-
-          <div>
-            <Label>Dependentes</Label>
-            <Input defaultValue={"0"} />
-          </div>
-
-          <div>
-            <TooltipProvider delayDuration={50}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Label>Abono pecuniário 1/3</Label>
-                </TooltipTrigger>
-                <TooltipContent className="max-w-[300px] text-sm">
-                  <p>
-                    Significa que você pode vender um terço (1/3) de suas férias
-                    para a empresa, recebendo uma remuneração a mais
-                  </p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-
-            <Select>
-              <SelectTrigger className="">
-                <SelectValue placeholder="Não" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="no">Não</SelectItem>
-                <SelectItem value="yes">Sim</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-        <div className="grid gap-y-4">
-          <div>
-            <Label>Média de hora extra</Label>
-            <div className="relative">
-              <div className="boder-[#CECED8] absolute inset-0 z-0 my-[1px] ml-[1px] flex h-[38px] w-10 items-center justify-center rounded border-0 border-y border-l bg-[#EEF2F8]">
-                <span className="text-[13px]">R$</span>
+          <div className="grid gap-y-4">
+            <div>
+              <Label>Média de hora extra</Label>
+              <div className="relative">
+                <div className="boder-[#CECED8] absolute inset-0 z-0 my-[1px] ml-[1px] flex h-[38px] w-10 items-center justify-center rounded border-0 border-y border-l bg-[#EEF2F8]">
+                  <span className="text-[13px]">R$</span>
+                </div>
+                <Input
+                  name="extra"
+                  className="z-10 pl-11"
+                  defaultValue={"0,00"}
+                />
               </div>
-              <Input className="z-10 pl-11" defaultValue={"0,00"} />
+            </div>
+
+            <div>
+              <Label>Dias de férias</Label>
+              <Input name="vacationDays" defaultValue={"0"} />
+            </div>
+
+            <div>
+              <Label>Adiantar 13 parcela?</Label>
+              <Select name="parcela13" defaultValue="no">
+                <SelectTrigger className="">
+                  <SelectValue placeholder="Não" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="no">Não</SelectItem>
+                  <SelectItem value="yes">Sim</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
-
-          <div>
-            <Label>Dias de férias</Label>
-            <Input defaultValue={"0"} />
-          </div>
-
-          <div>
-            <Label>Adiantar 13 parcela?</Label>
-            <Select>
-              <SelectTrigger className="">
-                <SelectValue placeholder="Não" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="no">Não</SelectItem>
-                <SelectItem value="yes">Sim</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
         </div>
-      </div>
-      <div className="mt-10 flex flex-col items-center gap-y-2">
-        <Link className="w-full" href="/resultado">
-          <Button className="w-full font-bold">Calcular</Button>
-        </Link>
-        <Button className="w-full font-medium text-[#5F5F5F]" variant={"ghost"}>
-          Limpar
-        </Button>
-      </div>
+        <div className="mt-10 flex flex-col items-center gap-y-2">
+          <Link
+            className="w-full"
+            href={{
+              href: "/resultado",
+              query: {
+                salary: 7,
+              },
+            }}
+          >
+            <Button className="w-full font-bold">Calcular</Button>
+          </Link>
+          <Button
+            className="w-full font-medium text-[#5F5F5F]"
+            variant={"ghost"}
+            type="submit"
+          >
+            Limpar
+          </Button>
+        </div>
+      </form>
     </section>
   )
 }
