a27ec869b3e1a271476f5bf6461510d21b1dfa13
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-05-20 21:01:15 -0300

    chore: setup jotai

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
+import { Providers } from "@/components/providers/jotai-provider"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -38,13 +39,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
-          <ThemeProvider attribute="class" defaultTheme="light">
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
+          <Providers>
+            <ThemeProvider attribute="class" defaultTheme="light">
+              <div className="relative flex min-h-screen flex-col">
+                <SiteHeader />
+                <div className="flex-1">{children}</div>
+              </div>
+              <TailwindIndicator />
+            </ThemeProvider>
+          </Providers>
         </body>
       </html>
     </>
