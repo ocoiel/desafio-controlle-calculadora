3a1d21f974bc9c0f2cf6f68e0d0e3b606b1537a9
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-05-21 21:17:02 -0300

    fix: 🎉 SEO improvemt and hydration error `<p>` tag fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,24 @@ export const metadata: Metadata = {
     default: "Controlle - Desafio",
     template: `%s`,
   },
+  description: "Calculadora de férias CLT - Desafio Controlle",
+  authors: {
+    name: "Gabriel Albuquerque",
+    url: "https://bento.me/gca",
+  },
+  keywords: ["Calculadora CLT", "Controlle"],
+  openGraph: {
+    title: "Desafio - Controlle",
+    actors: ["Gabriel Albuquerque"],
+    description: "Calculadora de férias CLT",
+    url: "https://www.controlle.com/assets/images/controlle.svg",
+  },
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
   },
 }
 
--- components/mobile-calc-sumary.tsx
+++ components/mobile-calc-sumary.tsx
@@ -4,48 +4,48 @@ export function MobileCalcSumary({ data }: { data: FormValues }) {
   return (
     <div className="grid grid-cols-2 gap-1 text-sm text-[#32323E] shadow-sm lg:hidden">
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Sálario Bruto:{" "}
           <p className="ml-1 font-semibold">R$ {data.salary},00</p>
-        </p>
+        </div>
       </div>
 
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Dependente: <p className="ml-1 font-semibold">{data.deps}</p>
-        </p>
+        </div>
       </div>
 
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Abono pecuniário:{" "}
           <p className="ml-1 font-semibold">
             {data.abono === "no" ? "Não" : "Sim"}
           </p>
-        </p>
+        </div>
       </div>
 
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Média de hora extra:{" "}
           <p className="ml-1 font-semibold">R$ {data.extra},00</p>
-        </p>
+        </div>
       </div>
 
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Dias de férias:{" "}
           <p className="ml-1 font-semibold">{data.vacationDays}</p>
-        </p>
+        </div>
       </div>
 
       <div className="block rounded-[6px] border border-[#E8ECF2] bg-white px-2 py-4 text-center">
-        <p>
+        <div>
           Adiantar 1ª parcela 13º:{" "}
           <p className="ml-1 font-semibold">
             {data.thirteenth === "no" ? "Não" : "Sim"}
           </p>
-        </p>
+        </div>
       </div>
     </div>
   )
